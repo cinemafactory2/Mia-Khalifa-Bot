3f5061ff5bbca6c78faee37091b5948ac2bf61b8
cinemafactory2 <73420844+cinemafactory2@users.noreply.github.com>
2020-11-21 12:01:02 +0530

    Update __main__.py

--- miakhalifa/__main__.py
+++ miakhalifa/__main__.py
@@ -28,7 +28,7 @@ from miakhalifa.modules.connection import connect_button
 
 PM_START_TEXT = """
 _Hello_ *{}*
-_My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
+_My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups me!!_
 _Maintained by_ [{}](tg://user?id={})
 """
 
